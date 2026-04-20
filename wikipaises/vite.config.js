1f8c30319c179e682d39bd318b3b1730f42ae731
gustavogurg <gustavogurgel@edu.unifor.br>
2026-04-20 16:09:24 -0300

    fix nome eraddo github pages

--- wikipaises/vite.config.js
+++ wikipaises/vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wikipaises/',
+  base: '/atividade_wikipaises/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
